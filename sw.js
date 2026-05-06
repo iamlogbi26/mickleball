46b7976e82b18cb0c7c2ed80986e6e7f3e3518d3
iamlogbi26 <coronadodaryl70@gmail.com>
2026-05-06 15:11:31 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,36 @@
+const CACHE_NAME = "mickleball-v1.02"; // Incremented version
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+// Install Event
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("pickleball").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Forces the new service worker to take over immediately
+});
+
+// Activate Event (Cleans up old versions of the app)
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
 
+// Fetch Event (Network First Strategy)
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
