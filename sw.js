b628380e82e629963aa4755d879119b0cce866bf
iamlogbi26 <coronadodaryl70@gmail.com>
2026-05-06 15:17:22 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,19 @@
-const CACHE_NAME = "mickleball-v1.02"; // Incremented version
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
-];
+const CACHE_NAME = "mickleball-force-v2";
 
-// Install Event
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting(); // Forces the new service worker to take over immediately
+  self.skipWaiting(); // FORCE the update
 });
 
-// Activate Event (Cleans up old versions of the app)
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
+      return Promise.all(keys.map(key => caches.delete(key))); // DELETE ALL OLD CACHE
     })
   );
 });
 
-// Fetch Event (Network First Strategy)
 self.addEventListener("fetch", e => {
+  // Always go to the internet first to get the newest code
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
